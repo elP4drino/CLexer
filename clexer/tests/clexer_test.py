6e41142a2210fb9506af2d628c576b1f050190ab
elP4drino <developerrv1024@gmail.com>
2024-04-14 10:40:42 -0600

    Finished

--- clexer/tests/clexer_test.py
+++ clexer/tests/clexer_test.py
@@ -17,7 +17,7 @@ class TestFloats(unittest.TestCase):
     def setUp(self):
         self.lexer = get_lexer()
 
-    def test_basic_floats(self):
+    def test_basic_floats2(self):
         self.lexer.input('15.75 1. .54')
         for value in [15.75, 1.0, 0.54]:
             token = self.lexer.token()
@@ -34,6 +34,13 @@ class TestFloats(unittest.TestCase):
     def test_edge_cases_floats(self):
         self.lexer.input('.0075e2 0.075e1 .075e1 75e-2')
         for value in [0.75, 0.75, 0.75, 0.75]:
+            token = self.lexer.token()
+            self.assertEqual(token.type, 'FLOAT')
+            self.assertEqual(token.value, value)
+            
+    def test_basic_floats(self):
+        self.lexer.input('15.75 1. .54')
+        for value in [15.75, 1.0, 0.54]:
             token = self.lexer.token()
             self.assertEqual(token.type, 'FLOAT')
             self.assertEqual(token.value, value)
@@ -59,6 +66,20 @@ class TestStrings(unittest.TestCase):
         token = self.lexer.token()
         self.assertEqual(token.type, 'STRING')
         self.assertEqual(token.value, '"escaped \\"quotes\\" and \\\\ backslashes \\\\"')
+
+
+class KeywordsTest(unittest.TestCase):
+    def setUp(self):
+        self.lexer = get_lexer()
+
+    def test_keywords(self):
+        self.lexer.input('auto break continue _Alignas')
+        expected_tokens = ['auto', 'break', 'continue', '_Alignas']
+        for expected_value in expected_tokens:
+            token = self.lexer.token()
+            print({token.type},{token.value})
+            self.assertEqual(token.type, 'KEYWORD')
+            self.assertEqual(token.value, expected_value)
 
 
 if __name__ == '__main__':
