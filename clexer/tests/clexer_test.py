cc0803b0013f857009f6669a7415794952203bd5
elP4drino <developerrv1024@gmail.com>
2024-04-14 08:47:28 -0600

    INT Rules Finished

--- clexer/tests/clexer_test.py
+++ clexer/tests/clexer_test.py
@@ -4,12 +4,12 @@ from src.clexer import get_lexer
 class TestIntegers(unittest.TestCase):
     def setUp(self):
         self.lexer = get_lexer()
-    
+
     def test_basic_integers(self):
-        self.lexer.input('1934')
+        self.lexer.input('90000000000004')
         token = self.lexer.token()
         self.assertEqual(token.type, 'INT')
-        self.assertEqual(token.value, 1934)
+        self.assertEqual(token.value, 90000000000004)
 
 if __name__ == '__main__':
     unittest.main()
