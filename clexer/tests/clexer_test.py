930a8f5baa3863053099f144eb3cccfb9a28ca17
elP4drino <developerrv1024@gmail.com>
2024-04-16 18:56:35 -0600

    Checking

--- clexer/tests/clexer_test.py
+++ clexer/tests/clexer_test.py
@@ -6,10 +6,10 @@ class TestIntegers(unittest.TestCase):
         self.lexer = get_lexer()
 
     def test_basic_integers(self):
-        self.lexer.input('034')
+        self.lexer.input('0x8a44000000000040')
         token = self.lexer.token()
         self.assertEqual(token.type, 'INT')
-        self.assertEqual(token.value, 0o34)
+        self.assertEqual(token.value, 0x8a44000000000040)
 
 if __name__ == '__main__':
     unittest.main()
